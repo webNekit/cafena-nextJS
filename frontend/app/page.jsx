58680dcb1f090e8b25b7f6b21241d0cf4b76d628
webNekit <mochalov-hunter@mail.ru>
2024-02-02 17:09:57 +0300

    Добавили размеры экраны для адаптивной верстки в файл tailwind.config.js

---
+++ frontend/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <h1 className="text-red-900 font-semibold text-[48px] bg-[#c4c4c4]">Text</h1>
+    </main>
+  );
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,22 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      '2xl': {'max': '1400px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1200px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1024px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '768px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '564px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 };
