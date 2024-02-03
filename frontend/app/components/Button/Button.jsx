8020b682b0fefde178d166a7bc8db043feb4a489
webNekit <mochalov-hunter@mail.ru>
2024-02-03 14:20:45 +0300

    Доделали секцию Hero и доработали компонент Button

--- frontend/app/components/Button/Button.jsx
+++ frontend/app/components/Button/Button.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 export default function Button({ children, href, type }) {
   if (href) {
     return(
-        <Link href={href} className={`${type === 'primary' ? 'bg-red-500' : 'bg-blue-500'}`}>{children}</Link>
+        <Link href={href} className={`${type === 'primary' ? "min-w-[70px] text-center px-3 py-2 text-white border border-zinc-900 bg-zinc-900" : 'min-w-[70px] text-center px-3 py-2 text-zinc-900 border border-yellow-500'}`}>{children}</Link>
     );
   }
-  return <button className={`${type === 'primary' ? 'bg-red-500' : 'bg-blue-500'}`}>{children}</button>
+  return <button className={`${type === 'primary' ? "min-w-[70px] text-center px-3 py-2 text-white border border-zinc-900 bg-zinc-900" : 'min-w-[70px] text-center px-3 py-2 text-zinc-900 border border-yellow-500'}`}>{children}</button>
 }
--- frontend/app/components/Hero/Hero.jsx
+++ frontend/app/components/Hero/Hero.jsx
@@ -6,15 +6,23 @@ export default function Hero() {
   return (
     <section className="w-full py-20">
       <Container> 
-        <div className="w-full">
+        <div className="w-full flex items-start">
             <div className="w-[60%]">
                 <h1 className="uppercase text-5xl leading-[70px]">ONE OF THE BEST COFFEE HOUSE IN YOUR HOME TOWN</h1>
                 <p className="text-base text-left mt-8 leading-8">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit do eiusmod veniam, quis nostrud exercitation
                     laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
-                <Button href="/" type="outline">Кнопка</Button>
+               <ul className="flex items-center gap-6 mt-12">
+                    <li className="inline-flex">
+                        <Button href="/" type="primary">Explore More</Button>
+                    </li>
+                    <li className="inline-flex">
+                        <Button href="/" type="outline">Read More</Button>
+                    </li>
+               </ul>
             </div>
+            <img className="object-cover" src="/hero_img.png" alt="Кофейные зерна" />
         </div>
       </Container>
     </section>
